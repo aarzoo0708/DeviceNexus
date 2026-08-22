38f8b6db4ac12609774c0f32fbb42fb31954cde1
Lovish Monga <lovish00070@gmail.com>
2026-08-22 21:12:17 +0530

    feat: add simple login modal and dynamic user personalization

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,17 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { RouterProvider } from "react-router/dom";
+import { RouterProvider } from "react-router";
 import router from "./router";
 import { ThemeProvider } from "./contexts/ThemeContext";
+import { AuthProvider } from "./contexts/AuthContext";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <ThemeProvider>
-      <RouterProvider router={router} />
+      <AuthProvider>
+        <RouterProvider router={router} />
+      </AuthProvider>
     </ThemeProvider>
   </StrictMode>
 );
